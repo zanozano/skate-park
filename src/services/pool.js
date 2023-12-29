a02dbe4696e76d0b67c346e17c8cab1834a3e376
Cristobal Andres Manzano Martinez <zano@live.cl>
2023-12-29 20:32:26 +0700

    update

--- src/services/pool.js
+++ src/services/pool.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    password: '0750manzano',
-    database: 'skatepark',
+    password: '123456',
+    database: 'tricktrack',
     port: 5432,
 });
 
